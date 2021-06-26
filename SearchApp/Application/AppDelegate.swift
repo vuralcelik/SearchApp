f35c371d297ec764c2f42814c62bb33445b0de09
Vural Çelik <vural.celik97@gmail.com>
2021-06-26 09:34:57 +0300

    -Initial project setup.

---
+++ SearchApp/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  SearchApp
+//
+//  Created by Vural Çelik on 26.06.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
